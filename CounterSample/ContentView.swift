acf1bcedb25112af2c2a82916f3cb1cefb0592cf
zaki-latte <ym.8844.flwease.developer@gmail.com>
2025-01-03 11:06:43 +0900

    Viewを実装

--- CounterSample/ContentView.swift
+++ CounterSample/ContentView.swift
@@ -7,13 +7,35 @@
 
 import SwiftUI
 
+@Observable
+final class ContentViewModel {
+    private(set) var count = 0
+
+    func incrementButtonTapped() {
+        count += 1
+    }
+
+    func decrementButtonTapped() {
+        count -= 1
+    }
+}
+
 struct ContentView: View {
+    @State private var viewModel = ContentViewModel()
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(viewModel.count)")
+
+            HStack {
+                Button("Increment") {
+                    viewModel.incrementButtonTapped()
+                }
+
+                Button("Decrement") {
+                    viewModel.decrementButtonTapped()
+                }
+            }
         }
         .padding()
     }
